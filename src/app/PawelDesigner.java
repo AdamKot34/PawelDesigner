4fb9c5979eb64b9236e0b9eb07b91e90e2ec76d9
Paweł Dudzik <adamkot34@gmail.com>
2017-09-27 18:31:19 +0200

    Dodanie obsługi wyjścia

--- src/app/PawelDesigner.java
+++ src/app/PawelDesigner.java
@@ -1,4 +1,4 @@
-﻿package app; //nazwa pakietu
+package app; //nazwa pakietu
 
 import java.util.Scanner; //import bibliotek
 
@@ -47,10 +47,29 @@ public class PawelDesigner {
 				ciepla = (35-temperatura)/10;
 				goraca = (temperatura-25)/10;
 			}
-			System.out.println("Zimna: "+zimna);
-			System.out.println("Letnia: "+letnia);
-			System.out.println("Ciepła: "+ciepla);
-			System.out.println("Gorąca: "+goraca);
+			double max1 = 0;
+			String x1 = "nieokreślona";
+			if (zimna > max1)
+			{
+				max1 = zimna;
+				x1 = "zimna";
+			}
+			if (letnia > max1)
+			{
+				max1 = letnia;
+				x1 = "letnia";
+			}
+			if (ciepla > max1)
+			{
+				max1 = ciepla;
+				x1 = "ciepła";
+			}
+			if (goraca > max1)
+			{
+				max1 = goraca;
+				x1 = "gorąca";
+			}
+			System.out.println("Temperatura jest "+x1+".");
 			System.out.println("Podaj wilgotność w procentach:");
 			double wilgotnosc = wejscie1.nextDouble();
 			double mala = 0, srednia = 0, duza = 0;
@@ -72,9 +91,53 @@ public class PawelDesigner {
 				srednia = (100-wilgotnosc)/50;
 				duza = (wilgotnosc-50)/50;
 			}
-			System.out.println("Mała: "+mala);
-			System.out.println("Średnia: "+srednia);
-			System.out.println("Duża: "+duza);
+			double max2 = 0;
+			String x2 = "nieokreślona";
+			if (mala > max2)
+			{
+				max2 = mala;
+				x2 = "mała";
+			}
+			if (srednia > max2)
+			{
+				max2 = srednia;
+				x2 = "średnia";
+			}
+			if (duza > max2)
+			{
+				max2 = duza;
+				x2 = "duża";
+			}
+			System.out.println("Wilgotność jest "+x2+".");
+			String y = "nieokreślona";
+			if (x1 == "zimna" && x2 == "duża"
+					|| x1 == "letnia" && x2 == "duża")
+			{
+				y = "zerowa";
+			}
+			if (x1 == "zimna" && x2 == "średnia"
+					|| x1 == "letnia" && x2 == "średnia"
+					|| x1 == "ciepła" && x2 == "duża")
+			{
+				y = "mała";
+			}
+			if (x1 == "zimna" && x2 == "mała"
+					|| x1 == "ciepła" && x2 == "średnia"
+					|| x1 == "gorąca" && x2 == "duża")
+			{
+				y = "średnia";
+			}
+			if (x1 == "letnia" && x2 == "mała" 
+					|| x1 == "ciepła" && x2 == "mała"
+					|| x1 == "gorąca" && x2 == "średnia")
+			{
+				y = "duża";
+			}
+			if (x1 == "gorąca" && x2 == "mała")
+			{
+				y = "maksymalna";
+			}
+			System.out.println("Intensywność podlewania jest "+y+".");
 			wejscie1.close();
 		} catch (Exception e) {
 			System.out.println("Błąd! Uruchom symulator ponownie.");
